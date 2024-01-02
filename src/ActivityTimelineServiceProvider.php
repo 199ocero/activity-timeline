6a204abc82022019e2038f9758ca7d3fbd456081
199ocero <199ocero@users.noreply.github.com>
2024-01-02 10:36:06 +0000

    Fix styling

--- src/ActivityTimelineServiceProvider.php
+++ src/ActivityTimelineServiceProvider.php
@@ -8,8 +8,6 @@ use Filament\Support\Assets\Css;
 use Filament\Support\Assets\Js;
 use Filament\Support\Facades\FilamentAsset;
 use Filament\Support\Facades\FilamentIcon;
-use Illuminate\Filesystem\Filesystem;
-use JaOcero\ActivityTimeline\Commands\ActivityTimelineCommand;
 use JaOcero\ActivityTimeline\Testing\TestsActivityTimeline;
 use Livewire\Features\SupportTesting\Testable;
 use Spatie\LaravelPackageTools\Commands\InstallCommand;
