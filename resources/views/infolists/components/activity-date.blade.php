85d4a1263c62b271b07ca0b3e7ad2334bdcb3536
sprtk-ches <145474359+sprtk-ches@users.noreply.github.com>
2024-01-24 16:40:35 +0000

    Support tooltips in the activity dates

--- resources/views/infolists/components/activity-date.blade.php
+++ resources/views/infolists/components/activity-date.blade.php
@@ -1,3 +1,15 @@
-<span class="flex-shrink-0 text-xs font-medium text-gray-500 uppercase fi-timeline-item-date dark:text-gray-400">
+@php
+    $tooltip = $getTooltip();
+@endphp
+
+<span
+    @if (filled($tooltip))
+        x-data="{}"
+        x-tooltip="{
+                content: @js($tooltip),
+                theme: $store.theme,
+            }"
+    @endif
+    class="flex-shrink-0 text-xs font-medium text-gray-500 uppercase fi-timeline-item-date dark:text-gray-400">
     {{ $getState() != null ? $getDate($getState()) : $getPlaceholder() }}
 </span>
