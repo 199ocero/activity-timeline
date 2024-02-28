5ff4ea51b31c9ec46de20bec07bd6018e8f94987
Daniel Silva <admin@puralogica.pt>
2024-02-28 01:49:53 +0000

    Add CanAllowHtml and CanModifyState
    Add CanAllowHtml and CanModifyState to ActivityDate to be able to add for example $date->diffForHumans() bellow date like "2 days ago"

--- resources/views/infolists/components/activity-date.blade.php
+++ resources/views/infolists/components/activity-date.blade.php
@@ -1,3 +1,7 @@
 <span class="flex-shrink-0 text-xs font-medium text-gray-500 uppercase fi-timeline-item-date dark:text-gray-400">
-    {{ $getState() != null ? $getDate($getState()) : $getPlaceholder() }}
+    @if ($isHtmlAllowed())
+        {!! $getModifiedState() ?? (!is_array($getState()) ? $getState() ?? $getPlaceholder() : null) !!}
+    @else
+        {{ $getModifiedState() ?? (!is_array($getState()) ? $getState() ?? $getPlaceholder() : null) }}
+    @endif
 </span>
